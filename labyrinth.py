3bd23ad6445006ccd66a0246cae4867b8775ef54
tresze <m.tresze@gmail.com>
2018-11-19 01:25:15 +0100

    PEP8, English comments and no more one-letter variable names

--- labyrinth.py
+++ labyrinth.py
@@ -1,6 +1,10 @@
-#labyrinth.py
+# labyrinth.py
 
-import os, sys, getch, contextlib, subprocess
+import os
+import sys
+import getch
+import contextlib
+import subprocess
 with contextlib.redirect_stdout(None):
     import pygame
     from random import randint
@@ -8,7 +12,8 @@ with contextlib.redirect_stdout(None):
 pygame.init()
 pygame.mixer.music.load("pop.wav")
 
-create = [sys.executable, "sub_file.py"]     # stablista letrehozasa
+create = [sys.executable, "sub_file.py"]    # creating the credits
+
 
 class bcolors:
     PURPLE = '\033[95m'
@@ -20,7 +25,8 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-# menu szovege
+
+# text of menu
 menu1 = "Please choose from the options:"
 menu2 = "Let's play! (p)"
 menu3 = "Credits (c)"
@@ -28,97 +34,103 @@ menu4 = "Quit (q)"
 
 filename = bcolors.GREEN + """
 
-                                                                         _           _                _       _   _     
-                                                                        | |         | |              (_)     | | | |    
-                                                                        | |     __ _| |__  _   _ _ __ _ _ __ | |_| |__  
-                                                                        | |    / _` | '_ \| | | | '__| | '_ \| __| '_ \ 
-                                                                        | |___| (_| | |_) | |_| | |  | | | | | |_| | | |
-                                                                        |______\__,_|_.__/ \__, |_|  |_|_| |_|\__|_| |_|
-                                                                                            __/ |                       
-                                                                                            |___/                        
+                                                                     _           _                _       _   _
+                                                                    | |         | |              (_)     | | | |
+                                                                    | |     __ _| |__  _   _ _ __ _ _ __ | |_| |__
+                                                                    | |    / _` | '_ \\| | | | '__| | '_ \\| __| '_ \\
+                                                                    | |___| (_| | |_) | |_| | |  | | | | | |_| | | |
+                                                                    |______\\__,_|_.__/ \\__, |_|  |_|_| |_|\\__|_| |_|
+                                                                                        __/ |
+                                                                                        |___/
                     """ + bcolors.ENDC
 
-# matrix keszitese a megadott fajlbol
+
+# creating a matrix from the given file
 def load_matrix(file_name):
-    m = []                # ebbe kerul majd a matrix
-    f = open(file_name)   # fajl megnyitasa
+    maze = []                # the matrix goes here
+    maze_file = open(file_name)     # opening the file
 
-    # soronkent vegigmegyunk a fajlon
-    for line in f:
-        line = line.strip()           # a sorbol toroljuk a whitespace karaktereket
-        line = list(line)             # listava alakitjuk a sor stringet
+    # iterating through the file line by line
+    for line in maze_file:
+        line = line.strip()    # deleting whitespace from the end of line
+        line = list(line)      # making a list out of a string line
 
-        m.append(line)   # beletesszuk a listat (sort) a matrixba
-    
-    # ellensegek generalasa
+        maze.append(line)    # putting the list (line) into the matrix
+
+    # generating enemies
     for i in range(0, 6):
-        # veletlen koordinata generalasa (nem a legszelso mezokhoz)
-        x = randint(1, len(m) - 2)
-        y = randint(1, len(m[0]) - 2)
+        # generating random coordinates (not on the edges)
+        row = randint(1, len(maze) - 2)
+        column = randint(1, len(maze[0]) - 2)
 
-        # folytatjuk, amig nem talalunk olyat, ahol ures hely van
-        while m[x][y] != "0":
-            x = randint(1, len(m) - 2)
-            y = randint(1, len(m[0]) - 2)
+        # continue until an empty space is found
+        while maze[row][column] != "0":
+            row = randint(1, len(maze) - 2)
+            column = randint(1, len(maze[0]) - 2)
 
-        # ha megvan, odateszunk egy ellenseget (plusz biztositva van, hogy az ellensegek ne zarjak el az utat)
-        if m[x-1][y] != "3" and m[x][y-1] != "3" and m[x+1][y] != "3" and m[x][y+1] != "3" and m[x-1][y-1] != "3" and m[x-1][y+1] != "3" and m[x+1][y+1] != "3" and m[x+1][y-1] != "3":
-            m[x][y] = "3"
+        # if an empty space is found, put an enemy there (plus the enemies won't block the circle's path)
+        if maze[row-1][column] != "3" and maze[row][column-1] != "3" and maze[row+1][column] != "3" and \
+           maze[row][column+1] != "3" and maze[row-1][column-1] != "3" and maze[row-1][column+1] != "3" and \
+           maze[row+1][column+1] != "3" and maze[row+1][column-1] != "3":
+                maze[row][column] = "3"
 
-        # es korbevesszuk az ellenseg koruli mezokkel
-        if m[x-1][y] == "0":
-            m[x-1][y] = "4"
-        if m[x][y-1] == "0":
-            m[x][y-1] = "4"
-        if m[x+1][y] == "0":
-            m[x+1][y] = "4"
-        if m[x][y+1] == "0":
-            m[x][y+1] = "4"
+        # surrounding the enemy with "fields next to the enemy" type fields
+        if maze[row-1][column] == "0":
+            maze[row-1][column] = "4"
+        if maze[row][column-1] == "0":
+            maze[row][column-1] = "4"
+        if maze[row+1][column] == "0":
+            maze[row+1][column] = "4"
+        if maze[row][column+1] == "0":
+            maze[row][column+1] = "4"
 
-    return m
+    return maze
 
-# a matrix elemei innen elerhetove valnak sor es oszlop alapjan; pl. a bal felso sarok igy: matrix[0][0]
+
+# the matrix's elements are reachable from now based on rows and columns; e.g. top-left corner looks like matrix[0][0]
 matrix = load_matrix("maze.txt")
 
-# ennek a valtozonak a segitsegevel nem lesz printelve az osszes karakter egyszerre (csak 310)
+# this variable makes sure that all of the characters won't get printed at once (just 310 of them)
 maze_slice = 310
 
-# labirintus es mozgathato karakter kirajzolasahoz fuggveny
+
+# definition for printing out the labyrinth and the moveable character
 def printmaze(matrix):
-    chars_count = 0     # kirajzolt karakterek megszamolasa
+    chars_count = 0     # counting the already printed characters
 
     for row in (matrix):
         for char in (row):
             if chars_count < maze_slice:
-                if char == "0" or char == "4":     # ures helyek, ellenseg melletti mezok
+                if char == "0" or char == "4":    # empty spaces, fields next to the enemy
                     sys.stdout.write("  ")
-                elif char == "1":                  # fal
+                elif char == "1":                 # wall
                     sys.stdout.write("〠")
-                elif char == "2":                  # a mozgathato karakter (kor)
+                elif char == "2":                 # moveable character (circle)
                     sys.stdout.write("◯ ")
-                elif char == "3":                  # ellenseg
+                elif char == "3":                 # enemy
                     sys.stdout.write("⚉ ")
-                elif char == "5":                 
+                elif char == "5":
                     sys.stdout.write("ↀ ")
                 chars_count += 1
         print()
 
-# matrix soraihoz es oszlopaihoz inicializalt valtozok
-i, j = 1, 1
+
+# variables initialized for the rows and columns of the matrix
+row, column = 1, 1
 
 # menu
-print("%s \n %110s \n %+102s \n %+100s \n %+99s" % (filename, menu1, menu2, menu3, menu4))
+print("%s \n %106s \n %+98s \n %+96s \n %+95s" % (filename, menu1, menu2, menu3, menu4))
 
 user_input = ""
 
 while user_input != "p" and user_input != "c" and user_input != "q":
     user_input = getch.getch()
 
-    if user_input == "p":                   # jatek inditasa es a labirintus kirajzolasa
+    if user_input == "p":      # starting the game and printing out the maze
         os.system("clear")
-        matrix[1][1] = "2"                  # a 2-es (kor) kezdopozicioja
+        matrix[1][1] = "2"     # the circle's starting position
         printmaze(matrix)
-    elif user_input == "c":                 # stablista
+    elif user_input == "c":    # credits
         subprocess.call(create)
         exit()
     elif user_input == "q":
@@ -126,30 +138,30 @@ while user_input != "p" and user_input != "c" and user_input != "q":
     else:
         print("Invalid input! Please enter \"p\", \"c\" or \"q\": ")
 
-# mozgas implementalasa
-while not (matrix[28][39] == "2" or matrix[29][39] == "2"): # ez a ket pozicio a labrinitus kijaratat jelenti
-    move = getch.getch()            # ennek a segitsegevel folyamatos a mozgas, nem kell mindig Entert leutni
-    print("\033[H\033[J")           # kepernyo frissitese
-    
-    matrix[i][j] = "0"              # ez a sor biztositja, hogy csak egy 2-es (kor) legyen egyszerre a labirintusban
+# implementing movement
+while not (matrix[28][39] == "2" or matrix[29][39] == "2"):    # these two positions mean the exit of the labyrinth
+    move = getch.getch()             # continuous movement, no pressing Enter needed
+    print("\033[H\033[J")            # updating the screen
 
-    if move == "a":                 # balra lepes = matrix[i][j-1] --> a jatekos nem valtoztatja a sort, az oszlopot viszont eggyel balra igen
-        if matrix[i][j-1] != "1":   # ha nem falra erkezik a kor
-            j = j - 1               # akkor egy pozicioval balrabb fogja kirajzolni azt
+    matrix[row][column] = "0"        # this line makes sure that only one circle is present at once in the labyrinth
+
+    if move == "a":    # move left = matrix[row][column-1] -> player is changing the column (-1), but not the row
+        if matrix[row][column-1] != "1":    # if the circle does not arrive on a wall
+            column = column - 1             # then print it out one position to the left
     if move == "s":
-        if matrix[i+1][j] != "1":
-            i = i + 1 
+        if matrix[row+1][column] != "1":
+            row = row + 1
     if move == "d":
-        if matrix[i][j+1] != "1":
-            j = j + 1 
+        if matrix[row][column+1] != "1":
+            column = column + 1
     if move == "w":
-        if matrix[i-1][j] != "1":
-            i = i - 1
+        if matrix[row-1][column] != "1":
+            row = row - 1
 
-    if matrix[i][j] == "4":
+    if matrix[row][column] == "4":
         print("Don't touch the enemy!\n")
 
-    if matrix[i][j] == "3":
+    if matrix[row][column] == "3":
         tryagain = ""
 
         while tryagain != "y" and tryagain != "Y" and tryagain != "n" and tryagain != "N":
@@ -162,13 +174,12 @@ while not (matrix[28][39] == "2" or matrix[29][39] == "2"): # ez a ket pozicio a
             else:
                 tryagain = input("Invalid input! Please enter \"y\" or \"n\": ")
 
-    if matrix[i][j] == "5":
+    if matrix[row][column] == "5":
         maze_slice += 310
 
-    matrix[i][j] = "2"               # az eppen aktualis pozicio valtson 0-rol, 3-rol vagy 4-rol 2-re (korre)
+    matrix[row][column] = "2"    # change the actual position (0, 3 or 4) to 2 (the circle)
     printmaze(matrix)
     pygame.mixer.music.play()
 
 else:
     print("Congratulations! You managed to get out of the labyrinth!")
-
